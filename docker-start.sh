acdeaa23bbd73a93f0a885b366cb58d6079b7e21
songshi_zhang <songshi_zhang@apple.com>
2024-10-19 17:59:55 +0800

    update

--- docker-start.sh
+++ docker-start.sh
@@ -4,8 +4,8 @@ python -m venv /venv
 . /venv/bin/activate
 #
 
-python -m pip install --upgrade pip -i https://mirrors.aliyun.com/pypi/simple/
-pip install -r requirements.txt -v -i https://mirrors.aliyun.com/pypi/simple/
+python -m pip install --upgrade pip -i https://mirrors.volces.com/pypi/simple/
+pip install -r requirements.txt -v -i https://mirrors.volces.com/pypi/simple/
 
 chmod +x /workspace/Docker/download.sh
 /workspace/Docker/download.sh
