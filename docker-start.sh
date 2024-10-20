2a5b662e31a4f80afb3f969eeffa91abc39f979c
wangxixi <wangxixi@chengfayun.com>
2024-10-20 12:57:58 +0800

    update

--- docker-start.sh
+++ docker-start.sh
@@ -10,6 +10,7 @@ pip install -r requirements.txt -v -i https://mirrors.volces.com/pypi/simple/
 chmod +x /workspace/Docker/download.sh
 /workspace/Docker/download.sh
 python -m nltk.downloader averaged_perceptron_tagger cmudict
+python /workspace/GPT_SoVITS/download.py
 
 # Set up environment variables
 ENV=${ENV:-"dev"}
